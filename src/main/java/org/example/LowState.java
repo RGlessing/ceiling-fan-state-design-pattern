00623490c8bd888946c317de7886b2e873fbfca9
Ryan Glessing <ry867625@dal.ca>
2022-07-27 21:25:38 -0300

    Implemented low to medium state change. Commented Code.

--- src/main/java/org/example/LowState.java
+++ src/main/java/org/example/LowState.java
@@ -1,8 +1,21 @@
 package org.example;
 
+/**
+ * This is the class file for a LowState object. The LowState implements the SpeedState interface. It contains
+ * the method changeSetting, that changes the fan speed from low to medium.
+ *
+ * @author Ryan Glessing
+ */
 public class LowState implements SpeedState {
+
+    /**
+     * When called, this method will use the setSpeed method of a CeilingFanContext object to change the current
+     * speed. The speed will be set to medium from low.
+     *
+     * @param context The Ceiling Fan Context
+     */
     @Override
     public void changeSetting(CeilingFanContext context) {
-
+        context.setSpeed(new MediumState());
     }
 }
--- src/main/java/org/example/OffState.java
+++ src/main/java/org/example/OffState.java
@@ -1,7 +1,20 @@
 package org.example;
 
+/**
+ * This is the class file for an OffState object. The OffState implements the SpeedState interface. It contains
+ * the method changeSetting, that changes the fan speed from off to low.
+ *
+ * @author Ryan Glessing
+ */
+
 public class OffState implements SpeedState{
 
+    /**
+     * When called, this method will use the setSpeed method of a CeilingFanContext object to change the current
+     * speed. The speed will be set to low from off.
+     *
+     * @param context The Ceiling Fan Context
+     */
     @Override
     public void changeSetting(CeilingFanContext context) {
         context.setSpeed(new LowState());
