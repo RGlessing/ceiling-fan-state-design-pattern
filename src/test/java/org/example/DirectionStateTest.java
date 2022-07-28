576bed9e7579871f810f407d4d4d079fdf5f1e8a
Ryan Glessing <ry867625@dal.ca>
2022-07-28 15:44:03 -0300

    Added and passed test to ensure direction change does not affect speed.

--- src/test/java/org/example/DirectionStateTest.java
+++ src/test/java/org/example/DirectionStateTest.java
@@ -30,5 +30,16 @@ public class DirectionStateTest {
         context.pullDirectionChain();
         assertTrue(context.getDirection() instanceof ClockwiseState);
     }
+    /**
+     * This test will ensure that the fan's speed is unaffected by a change in its direction when the direction chain
+     * is pulled. A new fan is initially off, and should remain off.
+     */
+    @Test
+    public void directionChangeDoesNotChangeSpeed(){
+        CeilingFanContext context = new CeilingFanContext();
+        context.pullDirectionChain();
+        assertTrue(context.getSpeed() instanceof OffState);
+    }
+
 
 }
