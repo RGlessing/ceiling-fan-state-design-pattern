90c4c7a6c07d0b746be44292ff689da235955b63
Ryan Glessing <ry867625@dal.ca>
2022-07-29 22:08:23 -0300

    Created OffStateTest with null state test.

---
+++ src/test/java/org/example/OffStateTest.java
@@ -0,0 +1,29 @@
+package org.example;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+public class OffStateTest {
+    private OffState offState;
+
+    @Rule
+    public final ExpectedException expectedException = ExpectedException.none();
+
+    @Before
+    public void before() {
+        this.offState = new OffState();
+    }
+
+    /**
+     * This test ensures the changeSetting method does not set the speed state to null
+     */
+    @Test
+    public void changeSetting_nullCeilingStateContext_throwsException() {
+        expectedException.expect(NullPointerException.class);
+        expectedException.expectMessage("context must be set");
+
+        this.offState.changeSetting(null);
+    }
+}
