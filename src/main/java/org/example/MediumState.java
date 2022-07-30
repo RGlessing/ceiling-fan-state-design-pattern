fa7b1690354932473cde0b2a3c9228f96299bfd0
Ryan Glessing <ry867625@dal.ca>
2022-07-29 22:19:50 -0300

    Passed MediumStateTest null context test.

--- src/main/java/org/example/MediumState.java
+++ src/main/java/org/example/MediumState.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import org.apache.commons.lang3.Validate;
+
 /**
  * This is the class file for a MediumState object. The MediumState implements the SpeedState interface. It contains
  * the method changeSetting, that changes the fan speed from medium to high.
@@ -17,6 +19,7 @@ public class MediumState implements SpeedState{
      */
     @Override
     public void changeSetting(CeilingFanContext context) {
+        Validate.notNull(context, "context must be set");
         context.setSpeed(new HighState());
     }
 }
