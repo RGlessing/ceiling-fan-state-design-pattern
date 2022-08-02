e2313786bc848fcdbb227c96445417298a699324
Ryan Glessing <ry867625@dal.ca>
2022-08-02 18:54:10 -0300

    Made demo more descriptive

--- src/main/java/org/example/Demo.java
+++ src/main/java/org/example/Demo.java
@@ -13,48 +13,48 @@ public class Demo {
          * Initialize Ceiling fan. Begin in off and clockwise states.
          */
         CeilingFanContext context = new CeilingFanContext();
-        System.out.println(context);
+        System.out.println("Initial Settings : " + context);
 
         /*
          * Pull speed chain. Change speed state from off to low.
          * Direction state remains in clockwise state.
          */
         context.pullSpeedChain();
-        System.out.println(context);
+        System.out.println("Speed Chain Pulled; Next Speed State : " + context);
 
         /*
          * Pull speed chain. Change speed state from low to medium.
          * Direction state remains in clockwise state.
          */
         context.pullSpeedChain();
-        System.out.println(context);
+        System.out.println("Speed Chain Pulled; Next Speed State : " + context);
 
         /*
          * Pull speed chain. Change speed state from medium to high.
          * Direction state remains in clockwise state.
          */
         context.pullSpeedChain();
-        System.out.println(context);
+        System.out.println("Speed Chain Pulled; Next Speed State : " + context);
 
         /*
          * Pull direction chain. Change direction state from clockwise to counterclockwise.
          * Speed state remains in high state.
          */
         context.pullDirectionChain();
-        System.out.println(context);
+        System.out.println("Direction Chain Pulled; Next Direction State : " + context);
 
         /*
          * Pull direction chain. Change direction state from counterclockwise to clockwise.
          * Speed state remains in high state.
          */
         context.pullDirectionChain();
-        System.out.println(context);
+        System.out.println("Direction Chain Pulled; Next Direction State : " + context);
 
         /*
          * Pull speed chain. Change speed state from high to off.
          * Direction state remains in clockwise state.
          */
         context.pullSpeedChain();
-        System.out.println(context);
+        System.out.println("Speed Chain Pulled; Next Speed State : " + context);
     }
 }
